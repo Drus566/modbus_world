aa491726bb9f58df6389cb1f29a74a83a8055571
Drus <zxandruxazx56@yandex.ru>
2025-08-15 16:49:47 +0500

    Обработка модбас пакета

--- src/modbus/linguist/ModbusLinguist.h
+++ src/modbus/linguist/ModbusLinguist.h
@@ -73,8 +73,8 @@ enum class ModbusRTU_Read {
 
 #define DWORD  uint32_t
 #define WORD   uint16_t
+#define BYTE   uint8_t
 #define BIT    uint8_t
-
 
 /** @brief типы данных карт */
 enum class MapType {
@@ -110,113 +110,153 @@ constexpr MemMode default_mem_mode = MemMode::LITTLE_ENDIAN_BYTE_SWAP_MODE;
 // (243) | 5401
 // (245) | 19084
 
-class Map {
+// 1,2,3,4 функции запрос
+struct RTU_READ_REQ {
+	WORD start_adr;
+	WORD quantity;
+	WORD crc;
+};
+
+// 5,6 функции
+struct RTU_WRITE_REQ {
+	WORD adr;
+	WORD val;
+	WORD crc;
+};
+
+// 15,16 функции
+struct RTU_WRITES_REQ {
+	union {
+		BYTE* byte_vals;
+		WORD* word_vals;
+	};
+	WORD start_adr;
+	WORD quantity;
+	WORD crc;
+	BYTE byte_count;
+};
+
+// 1,2,3,4 функции ответ
+struct RTU_READ_RESP {
+	union {
+		BYTE* byte_vals;
+		WORD* word_vals;
+	};
+	WORD crc;
+	BYTE count;
+};
+
+// 5,6 функции ответ
+struct RTU_WRITE_RESP {
+	WORD adr;
+	WORD val;
+	WORD crc;
+};
+
+// 15,16 функции ответ
+struct RTU_WRITES_RESP {
+	WORD start_adr;
+	WORD quantity;
+	WORD crc;
+};
+
+// Ответ ошибкой, подходит ко всем функциям
+struct RTU_ERROR_RESP {
+	WORD crc;
+	BYTE exception_code;
+};
+
+union RTU_PACKAGE {
+	BYTE slave;
+	BYTE func;
+
+	RTU_READ_REQ read_req;
+	RTU_WRITE_REQ write_req;
+	RTU_WRITES_REQ writes_req;
+	RTU_READ_RESP read_resp;
+	RTU_WRITE_RESP write_resp;
+	RTU_WRITES_RESP writes_resp;
+	RTU_ERROR_RESP error_resp;
+};
+
+
+
+class ModbusLinguist {
 public:
-	Map() : m_mem_16_ptr(nullptr), m_mem_8_ptr(nullptr) {}
-	Map(WORD start_adr, WORD quantity) : m_start_adr(start_adr), 
-													 m_quantity(quantity),
-													 m_mem_16_ptr(nullptr),
-													 m_mem_8_ptr(nullptr) {
-		m_end_adr = m_start_adr + quantity - 1;
-	}
-	~Map() {}
+	ModbusLinguist();
 
-	bool bindMap(MapType map_type, WORD start_adr, WORD quantity, void *data_ptr); // Привязка карты памяти к внешнему указателю на память (например на структуру пользователя)
-																										  // В этом случае чтение и запись в пользовательскую память необходимо будет производить
-																										  // только через картку памяти Map, если хотим безопасную работу с данными за счет mutex
-	
-	bool initNewMemory(WORD start_adr, WORD quantity, MapType map_type, MemMode mode = default_mem_mode); 		  // Инициализация новой памяти
-	bool initNewMemory(MapType map_type, MemMode mode = default_mem_mode); 		  
-	bool initNewMemory(); 														  
-	
-	void clearMemory();												// Очистка выделенной памяти
+	RTU_READ_RESP parseRTUReadResponse(char* package);
 
-	void setMapType(MapType map_type);							// Установка типа карты WORD или BIT
+	void parseRTUReqPackage(char* package) {
+		BYTE slave = *package;
+		BYTE func = *(package + 1);
+		switch (func) {
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+				WORD start_adr = *(package + 2);
+				WORD quantity = *(package + 4);
+				WORD crc = *(package + 6); 
+				break;
+			
+			case 5:
+			case 6:
+				WORD adr = *(package + 2);
+				WORD val = *(package + 4);
+				WORD crc = *(package + 6);
+				break;
 
-	// void setStartAdr(WORD start_adr);   						// Установка стартового адреса
-	// void setQuantity(WORD quantity);	 							// Установка количества регистров
-
-	/* Чтение|Запись битов и слов */
-	// Чтение слова
-	bool readWord(const WORD adr, WORD *const val, MemMode mode = default_mem_mode);
-	// Чтение двойного слова
-	bool readDWord(const WORD adr, DWORD *const val, MemMode mode = default_mem_mode);
-	// Чтение массива слов
-	bool readWords(const WORD adr, const WORD quantity, WORD *const val, MemMode mode = default_mem_mode);
-
-	// Запись слова
-	bool writeWord(const WORD adr, const WORD val, MemMode mode = default_mem_mode);
-	// Запись двойного слова
-	bool writeDWord(const WORD adr, const DWORD val, MemMode mode = default_mem_mode);
-	// Запись массива слов
-	bool writeWords(const WORD adr, const WORD quantity, WORD *const val, MemMode mode = default_mem_mode);
-
-	// Чтение указанного номера бита в слове, отсчет битов начинается с 0
-	bool readWordNBit(const WORD word_adr, const WORD bit_number, BIT *const val, MemMode mode = default_mem_mode);
-	// Чтение массива битов начиная с указанного номера бита в слове, отсчет битов начинается с 0
-	bool readWordNBits(const WORD word_adr, const WORD bit_number, const WORD quantity, BIT *const val, MemMode mode = default_mem_mode);
-	// Запись указанного номера бита в слове, отсчет битов начинается с 0
-	bool writeWordNBit(const WORD word_adr, const WORD bit_number, const BIT val, MemMode mode = default_mem_mode);
-	// Запись массива битов начиная с указанного номера бита в слове, отсчет битов начинается с 0
-	bool writeWordNBits(const WORD word_adr, const WORD bit_number, const WORD quantity, BIT *const val, MemMode mode = default_mem_mode);
-
-	// Чтение бита по адресу в карте слов, первый адрес бита равен 0, отсчет идет от 0 адреса в глобальном понимании
-	// Например есть WORD_MAP стартовый адрес = 7, количество = 10, тогда адрес первого бита слова будет считаться как 7 * 16 = 112 (слово по адресу 7)
-	bool readWordBit(const WORD adr, BIT* val, MemMode mode = default_mem_mode);
-	// Чтение массива битов по адресу в карте слов, первый адрес бита равен 0, отсчет идет от 0 адреса в глобальном понимании, и далее по порядку
-	bool readWordBits(const WORD adr, const WORD quantity, BIT *const val, MemMode mode = default_mem_mode);
-	// Запись бита по адресу в карте слов
-	bool writeWordBit(const WORD adr, const BIT val, MemMode mode = default_mem_mode);
-	// Запись битов по адресу в карте слов
-	bool writeWordBits(const WORD adr, const WORD quantity, BIT *const val, MemMode mode = default_mem_mode);
-
-	// Чтение бита
-	bool readBit(const WORD adr, BIT* val, MemMode mode = default_mem_mode);
-	// Чтение массива битов
-	bool readBits(const WORD adr, const WORD quantity, BIT *const val, MemMode mode = default_mem_mode);
-
-	// Запись бита
-	bool writeBit(const WORD adr, const BIT val, MemMode mode = default_mem_mode);
-	// Запись массива битов
-	bool writeBits(const WORD adr, const WORD quantity, BIT *const val, MemMode mode = default_mem_mode);
-
-	/* Чтение|Запись и интерпретирование в нужное представление int signed|int unsigned|float */
-	bool readUInt8(const WORD adr, uint8_t *const val, MemMode mode = default_mem_mode);
-	bool readUInt16(const WORD adr, uint16_t *const val, MemMode mode = default_mem_mode);
-	bool readUInt32(const WORD adr, uint32_t *const val, MemMode mode = default_mem_mode);
-	bool readInt8(const WORD adr, int8_t *const val, MemMode mode = default_mem_mode);
-	bool readInt16(const WORD adr, int16_t *const val, MemMode mode = default_mem_mode);
-	bool readInt32(const WORD adr, int32_t *const val, MemMode mode = default_mem_mode);
-	bool readFloat16(const WORD adr, float *const val, uint8_t precision = 1, MemMode mode = default_mem_mode);
-	bool readFloat32(const WORD adr, float *const val, MemMode mode = default_mem_mode);
-
-	bool writeUInt8(const WORD adr, const uint8_t val, MemMode mode = default_mem_mode);
-	bool writeUInt16(const WORD adr, const uint16_t val, MemMode mode = default_mem_mode);
-	bool writeUInt32(const WORD adr, const uint32_t val, MemMode mode = default_mem_mode);
-	bool writeInt8(const WORD adr, const int8_t val, MemMode mode = default_mem_mode);
-	bool writeInt16(const WORD adr, const int16_t val, MemMode mode = default_mem_mode);
-	bool writeInt32(const WORD adr, const int32_t val, MemMode mode = default_mem_mode);
-	bool writeFloat16(const WORD adr, const float val, uint8_t precision = 1, MemMode mode = default_mem_mode);
-	bool writeFloat32(const WORD adr, const float val, MemMode mode = default_mem_mode);
-
-	bool printBitMap(WORD width); // Вывод карты битов в консоль
-	bool printWordMap(WORD width); // Вывод карты слов в консоль
-	bool printWordMapBits(WORD width); // Вывод карты слов в консоль в битовом представлении
-
-	inline WORD helperWriteWordBit(WORD word, BIT bit_number, BIT bit_val) {
-		WORD result;
-		if (bit_val) result = word | (1 << bit_number); // Установка 1
-		else result = word & ~(1 << bit_number);		// Установка 0
-		return result;
+			case 15:
+			case 16:
+				WORD start_adr = *(package + 2);
+				WORD quantity = *(package + 4);
+				BYTE byte_count = *(package + 6);
+				WORD crc = *(package + 6 + byte_count + 1);
+		}
 	}
 
-	inline BIT helperReadWordBit(WORD word, BIT bit_number) {
-		return (word >> bit_number) & 1;	
+	void parseRTURespPackage(char* package) {
+		BYTE slave = *package;
+		BYTE func = *(package + 1);
+		switch (func) {
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+				WORD start_adr = *(package + 2);
+				WORD quantity = *(package + 4);
+				WORD crc = *(package + 6); 
+				break;
+			
+			case 5:
+			case 6:
+				WORD adr = *(package + 2);
+				WORD val = *(package + 4);
+				WORD crc = *(package + 6);
+				break;
+
+			case 15:
+			case 16:
+				WORD start_adr = *(package + 2);
+				WORD quantity = *(package + 4);
+				BYTE byte_count = *(package + 6);
+				WORD crc = *(package + 6 + byte_count + 1);
+				break;
+			
+			case 129:
+			case 130:
+			case 131:
+			case 132:
+			case 133:
+			case 134:
+			case 143:
+			case 144:
+				BYTE exception_code = *(package + 2);
+				WORD crc = *(package + 3);
+		}
 	}
 
-	inline WORD helperInvertWordBit(WORD word, BIT bit_number) {
-		return word = word ^ (1 << bit_number);
-	}
 
 
 private:
