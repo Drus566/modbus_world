190f9960979d5a5142f19f32173d61d441744f0b
Drus566 <zxandruxazx56@yandex.ru>
2025-08-19 17:02:49 +0500

    Работа над Serial

--- src/interaction/serial/Serial.cpp
+++ src/interaction/serial/Serial.cpp
@@ -28,17 +28,15 @@ void Serial::init(std::string device, int baud, char parity, int data_bit, int s
 		std::cout << "Device string is empty, then set to " << DEFAULT_PORT << std::endl;
 		m_device = DEFAULT_PORT;
 	}
-	else
-		m_device = device;
+	else m_device = device;
 
 	if (baud <= 0) {
 		std::cout << "Baudrate value is <= 0, then set to " << DEFAULT_BAUD << std::endl;
 		m_baudrate = DEFAULT_BAUD;
 	}
+	else m_baudrate = baud;
 
-	if (parity == 'N' || parity == 'E' || parity == 'O') {
-		m_parity = parity;
-	}
+	if (parity == 'N' || parity == 'E' || parity == 'O') m_parity = parity;
 	else {
 		m_parity = 'N';
 		std::cout << "Parity is incorrect, then set to " << DEFAULT_PARITY << std::endl;
@@ -46,9 +44,15 @@ void Serial::init(std::string device, int baud, char parity, int data_bit, int s
 
 	m_data_bit = data_bit;
 	m_stop_bit = stop_bit;
+
+	m_serial_mode = SerialMode::NONE;
+
+	m_rts = RtsMode::NONE;
+	m_onebyte_time = 1000000 * (1 + m_data_bit + (m_parity == 'N' ? 0 : 1) + m_stop_bit) / m_baudrate;
+	m_rts_delay = m_onebyte_time;
 }
 
-int Serial::connect() {
+bool Serial::connect() {
 	struct termios tios;
 	int flags;
 	speed_t speed;
@@ -56,7 +60,7 @@ int Serial::connect() {
 	if (m_debug) {
 		std::cout << "Opening "<< m_device << " at " 
 		<< m_baudrate << " bauds (" << m_parity << ", " 
-		<< (int)m_data_bit << ", " << m_stop_bit << ")" << std::endl;
+		<< (int)m_data_bit << ", " << (int)m_stop_bit << ")" << std::endl;
 	}
 
 	flags = O_RDWR | O_NOCTTY | O_NONBLOCK | O_EXCL;
@@ -64,21 +68,21 @@ int Serial::connect() {
 
 	if (m_fd < 0) {
 		if (m_debug) {
-			std::cout << "Error open device " << m_device << " (" << strerror(errno) << ")";
+			std::cout << "Error open device " << m_device << " (" << strerror(errno) << ")" << std::endl;;
 		}
-		return -1;
+		return false;
 	}
 
 	tcgetattr(m_fd, &m_old_tios);
 	memset(&tios, 0, sizeof(termios));
 
 	if (9600 == B9600) speed = m_baudrate;
-	else speed = getTermiosSpeed(m_baudrate);
+	else speed = Serial::getTermiosSpeed(m_baudrate, m_debug);
 
 	if ((cfsetispeed(&tios,speed) < 0) || (cfsetospeed(&tios,speed) < 0)) {
 		close(m_fd);
 		m_fd = 1;
-		return -1;
+		return false;
 	}
 
 	tios.c_cflag |= (CREAD | CLOCAL);
@@ -127,7 +131,7 @@ int Serial::connect() {
 	if (tcsetattr(m_fd, TCSANOW, &tios) < 0) {
 		close(m_fd);
 		m_fd = -1;
-		return -1;
+		return false;
 	}
 
 	return 0;
@@ -139,152 +143,101 @@ long Serial::send(const uint8_t* req, int req_length) {
 	return write(m_fd, req, req_length);
 }
 
-long Serial::doReceive(uint8_t* rsp, int rsp_length) {
-	return read(ctx->s, rsp, rsp_length);
-}
+long Serial::sendRts(const uint8_t* req, int req_length) {
+	ssize_t size;
 
-/* Waits a response from a modbus server or a request from a modbus client.
-	This function blocks if there is no replies (3 timeouts).
-
-	The function shall return the number of received characters and the received
-	message in an array of uint8_t if successful. Otherwise it shall return -1
-	and errno is set to one of the values defined below:
-	- ECONNRESET
-	- EMBBADDATA
-	- ETIMEDOUT
-	- read() or recv() error codes
-*/
-
-int Serial::waitReceive(uint8_t *msg) {
-	int rc;
-	fd_set rset;
-	struct timeval tv;
-	struct timeval *p_tv;
-	unsigned int length_to_read;
-	int msg_length = 0;
-
-	if (!isConnected()) {
-		if (m_debug) {
-			std::cout << "ERROR The connection is not established" << std::endl;
-		}
-		return -1;
+	if (m_debug) {
+		std::cout << "Sending request using RTS signal\n" << std::endl;
 	}
 
-	/* Add a file descriptor to the set */
-	FD_ZERO(&rset);
-	FD_SET(m_fd, &rset);
+	invertRts(m_rts == RtsMode::UP);
+	usleep(m_rts_delay);
 
-	int ret = 0;
-	while (length_to_read != 0) {
-		rc = doSelect(&rset, p_tv, length_to_read);
-		if (rc == -1) {
-			std::cout << "doSelect error" << std::endl;
-			return -1;
-		}
+	size = write(m_fd, req, req_length);
 
-		rc = doReceive(msg + msg_length, length_to_read);
-		if (rc == 0)
-		{
-			errno = ECONNRESET;
-			rc = -1;
-		}
+	usleep(m_onebyte_time * req_length + m_rts_delay);
+	invertRts(m_rts != RtsMode::UP);
 
-		if (rc == -1)
-		{
-			// _error_print(ctx, "read");
-			// if ((ctx->error_recovery & MODBUS_ERROR_RECOVERY_LINK) &&
-			// 	 (errno == ECONNRESET || errno == ECONNREFUSED || errno == EBADF))
-			// {
-			// 	int saved_errno = errno;
-			// 	modbus_close(ctx);
-			// 	modbus_connect(ctx);
-			// 	/* Could be removed by previous calls */
-			// 	errno = saved_errno;
-			// }
-		}
+	return size;
+}
 
-		/* Display the hex code of each character received */
-		if (m_debug)
-		{
-			int i;
-			for (i = 0; i < rc; i++)
-				printf("<%.2X>", msg[msg_length + i]);
-		}
+long Serial::receive(uint8_t* rsp, int rsp_length) {
+	return read(m_fd, rsp, rsp_length);
+}
 
-		// if (ctx->log)
-		// {
-		// 	int i;
-		// 	for (i = 0; i < rc; i++)
-		// 	{
-		// 		ret += sprintf(ctx->rx_log_buffer + ret, "<%.2X>", msg[msg_length + i]);
-		// 	}
-		// 	// sprintf(ctx->rx_log_buffer + ret, "\n");
-
-		// 	// sprintf(ctx->log_buffer + ret, "\n");
-		// }
-
-		/* Sums bytes received */
-		msg_length += rc;
-		/* Computes remaining bytes */
-		length_to_read -= rc;
-
-		if (length_to_read == 0)
-		{
-			// switch (step)
-			// {
-			// case _STEP_FUNCTION:
-			// 	/* Function code position */
-			// 	length_to_read = compute_meta_length_after_function(
-			// 		 msg[ctx->backend->header_length], msg_type);
-			// 	if (length_to_read != 0)
-			// 	{
-			// 		step = _STEP_META;
-			// 		break;
-			// 	} /* else switches straight to the next step */
-			// case _STEP_META:
-			// 	length_to_read = compute_data_length_after_meta(ctx, msg, msg_type);
-			// 	if ((msg_length + length_to_read) > ctx->backend->max_adu_length)
-			// 	{
-			// 		errno = EMBBADDATA;
-			// 		_error_print(ctx, "too many data");
-			// 		return -1;
-			// 	}
-			// 	step = _STEP_DATA;
-			// 	break;
-			// default:
-			// 	break;
-			// }
-		}
-
-		if (length_to_read > 0 && (m_byte_timeout.tv_sec > 0 || m_byte_timeout.tv_usec > 0)) {
-			/* If there is no character in the buffer, the allowed timeout
-				interval between two consecutive bytes is defined by
-				byte_timeout */
-			tv.tv_sec = m_byte_timeout.tv_sec;
-			tv.tv_usec = m_byte_timeout.tv_usec;
-			p_tv = &tv;
-		}
-		/* else timeout isn't set again, the full response must be read before
-			expiration of response timeout (for CONFIRMATION only) */
+bool Serial::setSerialMode(SerialMode mode) {
+	if (m_fd < 0) {
+		errno = EINVAL;
+		std::cout << "Serial: Error setSerialMode, Device socket " << m_fd << " not opened" << std::endl;
+		return false;
 	}
 
-	// if (ctx->debug)
-	// 	printf("\n");
+	if (!Serial::isSerialSupported(m_fd)) {
+		if (errno == ENOTSUP) {
+			std::cout << "Serial: Serial mode not supported" << std::endl;
+		}
+		else {
+			std::cout << "Serial: ioctl error call" << std::endl;
+		}
+		return false;
+	}
 
-	// if (ctx->log) {
-	//     sprintf(ctx->rx_log_buffer, "\n");
-	// }
+	serial_rs485 rs485_conf;
+	if (mode == SerialMode::RS485) {
+		// Get
+		if (ioctl(m_fd, TIOCGRS485, &rs485_conf) < 0) return false;
+		
+		// Set
+		rs485_conf.flags |= SER_RS485_ENABLED;
+		if (ioctl(m_fd, TIOCSRS485, &rs485_conf) < 0) return false;
 
-	return ctx->backend->check_integrity(ctx, msg, msg_length);
+		m_serial_mode = SerialMode::RS485;
+	}
+	else if (mode == SerialMode::RS232) {
+		/* Turn off RS485 mode only if required */
+		if (m_serial_mode == SerialMode::RS485) {
+			/* The ioctl call is avoided because it can fail on some RS232 ports */
+			if (ioctl(m_fd, TIOCGRS485, &rs485_conf) < 0) return false;
+			
+			rs485_conf.flags &= ~SER_RS485_ENABLED;
+			if (ioctl(m_fd, TIOCSRS485, &rs485_conf) < 0) return false;
+		}
+		m_serial_mode = SerialMode::RS232;
+	}
+	return true;
 }
 
-void Serial::setRts(bool flag) {
+bool Serial::setRts(RtsMode mode) {
+	if (m_fd <= 0) {
+		errno = EINVAL;
+		std::cout << "Serial: Error setRts, Device socket " << m_fd << " not opened" << std::endl;
+		return false;
+	}
 
+	if (!Serial::isRtsSupported(m_fd)) {
+		std::cout << "Serial: Rts mode not supported" << std::endl;
+		return false;
+	}
+
+	if (mode == RtsMode::NONE || mode == RtsMode::UP || mode == RtsMode::DOWN) {
+		m_rts = mode;
+		invertRts(m_rts != RtsMode::UP);
+	}
 }
 
-int Serial::flush() { return tcflush(m_fd, TCIOFLUSH); }
+bool Serial::invertRts(bool flag) {
+	int fd = m_fd;
+	int flags;
 
-int Serial::doSelect(fd_set* rset, timeval* tv, int length_to_read) {
+	ioctl(fd, TIOCMGET, &flags);
+	if (flag) flags |= TIOCM_RTS;
+	else flags &= ~TIOCM_RTS;
+	ioctl(fd, TIOCMSET, &flags);
+}
+
+bool Serial::flush() { return tcflush(m_fd, TCIOFLUSH) == 0; }
+
+bool Serial::doSelect(fd_set* rset, timeval* tv) {
 	int s_rc;
 	while ((s_rc = select(m_fd + 1, rset, NULL, NULL, tv)) == -1) {
 		if (errno == EINTR) {
@@ -293,27 +246,50 @@ int Serial::doSelect(fd_set* rset, timeval* tv, int length_to_read) {
 			FD_ZERO(rset);
 			FD_SET(m_fd, rset);
 		}
-		else return -1;
+		else return false;
 	}
 
 	if (s_rc == 0) {
 		/* Timeout */
 		errno = ETIMEDOUT;
-		return -1;
+		return false;
 	}
-	return 0;
+	return true;
 }
 
-int Serial::closeSocket() {
+void Serial::doClose() {
 	if (m_fd >= 0) {
 		tcsetattr(m_fd, TCSANOW, &m_old_tios);
 		close(m_fd);
 		m_fd = -1;
 	}
-	return 0;
 }
 
-static speed_t getTermiosSpeed(int baud, int debug) {
+bool Serial::isRtsSupported(int socket) {
+	bool result = true;
+	int status;
+	if (ioctl(socket, TIOCMGET, &status) == -1) {
+		result = false;
+		errno = ENOTSUP;
+	}
+	return result;
+}
+
+bool Serial::isSerialSupported(int socket) {
+	bool result = true;
+	struct serial_rs485 rs485_conf;
+	memset(&rs485_conf, 0, sizeof(rs485_conf));
+	int ret = ioctl(socket, TIOCGRS485, &rs485_conf);
+	if (ret < 0) {
+		if (errno == ENOTTY || errno == EINVAL) {
+			errno = ENOTSUP;
+		}
+		result = false;
+	}
+	return result;
+}
+
+speed_t Serial::getTermiosSpeed(int baud, bool debug) {
 	speed_t speed;
 
 	switch (baud) {
@@ -421,6 +397,118 @@ static speed_t getTermiosSpeed(int baud, int debug) {
 
 	return speed;
 }
+
+/* Waits a response from a modbus server or a request from a modbus client.
+	This function blocks if there is no replies (3 timeouts).
+
+	The function shall return the number of received characters and the received
+	message in an array of uint8_t if successful. Otherwise it shall return -1
+	and errno is set to one of the values defined below:
+	- ECONNRESET
+	- EMBBADDATA
+	- ETIMEDOUT
+	- read() or recv() error codes
+*/
+
+// int Serial::waitReceive(uint8_t *msg, int length_to_read) {
+// 	int rc;						// результат выполнения
+// 	fd_set rset;				// дескрипторы для select
+// 	struct timeval tv;		// время для таймаутов
+// 	struct timeval *p_tv;	// указатель на время для таймаутов
+// 	int msg_length = 0;		// считано байтов
+
+// 	// Проверка соединения
+// 	if (!isConnected()) {
+// 		if (m_debug) {
+// 			std::cout << "ERROR The connection is not established" << std::endl;
+// 		}
+// 		return -1;
+// 	}
+
+// 	/* Add a file descriptor to the set */
+// 	FD_ZERO(&rset);
+// 	FD_SET(m_fd, &rset);
+
+// 	int ret = 0;
+// 	while (length_to_read != 0) {
+// 		rc = doSelect(&rset, p_tv);
+// 		if (rc == -1) {
+// 			std::cout << "doSelect error" << std::endl;
+// 			return -1;
+// 		}
+
+// 		rc = doReceive(msg + msg_length, length_to_read);
+// 		if (rc == 0) {
+// 			errno = ECONNRESET;
+// 			rc = -1;
+// 		}
+
+// 		if (rc == -1) {
+// 			// _error_print(ctx, "read");
+// 			if ((ctx->error_recovery & MODBUS_ERROR_RECOVERY_LINK) &&
+// 				 (errno == ECONNRESET || errno == ECONNREFUSED || errno == EBADF))
+// 			{
+// 				closeSocket();
+// 				// int saved_errno = errno;
+// 				// modbus_close(ctx);
+// 				// modbus_connect(ctx);
+// 				/* Could be removed by previous calls */
+// 				// errno = saved_errno;
+// 			}
+// 		}
+
+// 		/* Display the hex code of each character received */
+// 		if (m_debug) {
+// 			int i;
+// 			for (i = 0; i < rc; i++)
+// 				printf("<%.2X>", msg[msg_length + i]);
+// 		}
+
+// 		/* Sums bytes received */
+// 		msg_length += rc;
+// 		/* Computes remaining bytes */
+// 		length_to_read -= rc;
+
+// 		if (length_to_read == 0) {
+// 			// switch (step)
+// 			// {
+// 			// case _STEP_FUNCTION:
+// 			// 	/* Function code position */
+// 			// 	length_to_read = compute_meta_length_after_function(
+// 			// 		 msg[ctx->backend->header_length], msg_type);
+// 			// 	if (length_to_read != 0)
+// 			// 	{
+// 			// 		step = _STEP_META;
+// 			// 		break;
+// 			// 	} /* else switches straight to the next step */
+// 			// case _STEP_META:
+// 			// 	length_to_read = compute_data_length_after_meta(ctx, msg, msg_type);
+// 			// 	if ((msg_length + length_to_read) > ctx->backend->max_adu_length)
+// 			// 	{
+// 			// 		errno = EMBBADDATA;
+// 			// 		_error_print(ctx, "too many data");
+// 			// 		return -1;
+// 			// 	}
+// 			// 	step = _STEP_DATA;
+// 			// 	break;
+// 			// default:
+// 			// 	break;
+// 			// }
+// 		}
+
+// 		if (length_to_read > 0 && (m_byte_timeout.tv_sec > 0 || m_byte_timeout.tv_usec > 0)) {
+// 			/* If there is no character in the buffer, the allowed timeout
+// 				interval between two consecutive bytes is defined by
+// 				byte_timeout */
+// 			tv.tv_sec = m_byte_timeout.tv_sec;
+// 			tv.tv_usec = m_byte_timeout.tv_usec;
+// 			p_tv = &tv;
+// 		}
+// 		/* else timeout isn't set again, the full response must be read before
+// 			expiration of response timeout (for CONFIRMATION only) */
+// 	}
+// }
+
 
 // 	// long send()
 // 	// {
--- src/interaction/serial/Serial.h
+++ src/interaction/serial/Serial.h
@@ -9,6 +9,18 @@
 namespace mb {
 namespace interaction {
 
+enum class RtsMode {
+	NONE = 0,
+	UP   = 1,
+	DOWN = 2
+};
+
+enum class SerialMode {
+	NONE  = 0,
+	RS232 = 1,
+	RS485 = 2
+};
+
 /* Timeouts in microsecond (0.5 s) */
 #define _RESPONSE_TIMEOUT 500000
 #define _BYTE_TIMEOUT     500000
@@ -17,37 +29,68 @@ class Serial {
 public:
 	Serial();
 	Serial(std::string device, int baud, char parity, int data_bit, int stop_bit);
-	~Serial();
 
+	// Инициализация
 	void init(std::string device, int baud, char parity, int data_bit, int stop_bit);
-	int connect();
+	// Подключение
+	bool connect();
+	// Статус подключения
 	bool isConnected();
+	// Отправка сообщения
 	long send(const uint8_t *req, int req_length);
-	int waitReceive(uint8_t *msg);
-	long doReceive(uint8_t *rsp, int rsp_length);
-	void setRts(bool flag);
-	int flush();
-	// _modbus_rtu_select(modbus_t *ctx, fd_set *rset, struct timeval *tv, int length_to_read)
-	int doSelect(fd_set *rset, struct timeval *tv, int length_to_read);
-	int closeSocket();
+	// Отправка сообщения с помощью rts
+	long sendRts(const uint8_t *req, int req_length);
+	// Получение сообщения
+	long receive(uint8_t *rsp, int rsp_length);
+	// Установка аппаратного управления потока данных
+	bool setRts(RtsMode mode);
+	// Переключить аппаратное управление потока данных
+	bool invertRts(bool flag);
+	// Установка режима для аппаратной части RS232/RS485 (если поддерживается)
+	bool setSerialMode(SerialMode mode);
+	// Очистка буфера ввода (принимаемые данные) вывода (не отправленные данные)
+	bool flush();
+	// Ожидание файлового дескриптора на ввод данных
+	bool doSelect(fd_set *rset, struct timeval *tv);
+	// Закрыть файловый дескриптор
+	void doClose();
 
-	static speed_t getTermiosSpeed(int baud);
+	// Проверка поддержки аппаратного управления
+	static bool isSerialSupported(int socket);
+	// Проверка поддержки аппаратного управления RTS
+	static bool isRtsSupported(int socket);
+	// Получение скорости
+	static speed_t getTermiosSpeed(int baud, bool debug);
 
 private:
+	// Дескриптор устройства
 	int m_fd;
 
+	// Путь к устройству
 	std::string m_device;
+	// Скорость в бодах
 	int m_baudrate;
+	// Четность
 	char m_parity;
+	// Биты данных
 	uint8_t m_data_bit;
+	// Стоп бит
 	uint8_t m_stop_bit;
 
-	timeval m_response_timeout;
-	timeval m_byte_timeout;
-	timeval m_indication_timeout;
-
+	// Для сохранения старых настроек последовательного порта
 	termios m_old_tios;
 
+	// Режим rts (если поддераживается)
+	RtsMode m_rts;
+	// Задержка rts
+	int m_rts_delay;
+	// Интервал передачи одного байта в rts
+	int m_onebyte_time;
+
+	// Режим последовательного порта (если поддерижвается)
+	SerialMode m_serial_mode;
+
+	// Отладочные сообщения 
 	bool m_debug;
 };
 
